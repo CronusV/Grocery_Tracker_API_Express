9233c04147cfdc91d9c4f2c5bd8f7aab4a368ec8
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-19 13:54:57 -0700

    Added update to dao

--- repository/groceryDAO.js
+++ repository/groceryDAO.js
@@ -54,9 +54,58 @@ function deleteGroceryItemByID(grocery_id) {
   };
   return docClient.delete(params).promise();
 }
+
+// UPDATE item with grocery id
+function updateGroceryItemByID(grocery_id, item) {
+  const ExpressionAttributeNames = {};
+  const ExpressionAttributeValues = {};
+  const expressions = [];
+  // Going to update if req body has correct key value and not empty
+  if (item.name !== undefined && item.name !== '') {
+    ExpressionAttributeNames['#name'] = 'name';
+    ExpressionAttributeValues[':name'] = item.name;
+    expressions.push('#name = :name');
+  }
+  if (item.quantity !== undefined && item.quantity !== '') {
+    ExpressionAttributeNames['#quantity'] = 'quantity';
+    ExpressionAttributeValues[':quantity'] = item.quantity;
+    expressions.push('#quantity = :quantity');
+  }
+  if (item.price !== undefined && item.price !== '') {
+    ExpressionAttributeNames['#price'] = 'price';
+    ExpressionAttributeValues[':price'] = item.price;
+    expressions.push('#price = :price');
+  }
+  if (item.bought !== undefined && item.bought !== '') {
+    ExpressionAttributeNames['#bought'] = 'bought';
+    ExpressionAttributeValues[':bought'] = item.bought;
+    expressions.push('#bought = :bought');
+  }
+  if (item.category !== undefined && item.category !== '') {
+    ExpressionAttributeNames['#category'] = 'category';
+    ExpressionAttributeValues[':category'] = item.category;
+    expressions.push('#category = :category');
+  }
+  // Join expressions into one to update what's necessary
+  const UpdateExpression = `set ${expressions.join(', ')}`;
+
+  const params = {
+    TableName,
+    Key: {
+      grocery_id,
+    },
+    UpdateExpression,
+    ExpressionAttributeNames,
+    ExpressionAttributeValues,
+  };
+
+  console.log(`In updateDAO with params:\n${JSON.stringify(params)}`);
+  return docClient.update(params).promise();
+}
 module.exports = {
   addGroceryItem,
   getGroceryItemByID,
   retrieveAllGroceryItems,
   deleteGroceryItemByID,
+  updateGroceryItemByID,
 };
