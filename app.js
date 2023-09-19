b0e60a6567839e4322a99e4653a2820ecf2bc7d6
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-19 11:30:18 -0700

    Added router with /api

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const groceryDAO = require('./repository/groceryDAO');
+// Logger
+const { createLogger, transports, format } = require('winston');
 // Init Express server
 const express = require('express');
 const server = express();
 const PORT = 3000;
-//grocery_id creator
-const uuid = require('uuid');
-
+// router
+const APIRouter = require('./routes/APIRouter');
 //middleware to parse JSON from req
 const bodyParser = require('body-parser');
 server.use(bodyParser.json());
+// Using router with base url
+server.use('/api', APIRouter);
+// Logger init
+const logger = createLogger({
+  level: 'info', // this will log only messages with the level 'info' and above
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(), // log to the console
+    new transports.File({ filename: 'app.log' }), // log to a file
+  ],
+});
 
 // Test
 server.get('/', (req, res) => {
+  console.log(req);
   console.log('Hello world, this works in get using Express!');
   groceryDAO
     .addGroceryItem(uuid.v4(), 'strawberry', 5, 20, true, 'fruit')
@@ -26,8 +44,9 @@ server.get('/', (req, res) => {
       res.send({ message: 'Failed to Add Item', err });
     });
 });
-// POST
 
+// POST
+// Done with router
 // GET
 
 // PUT
@@ -38,3 +57,5 @@ server.get('/', (req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is listening on Port: ${PORT}`);
 });
+
+module.exports = { logger };
