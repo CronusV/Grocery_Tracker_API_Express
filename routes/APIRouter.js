727bd207ee74e03bd3147091f3e29b3dd6475131
CronusV <33589161+CronusV@users.noreply.github.com>
2023-09-27 18:45:50 -0700

    styling

--- routes/APIRouter.js
+++ routes/APIRouter.js
@@ -160,12 +160,10 @@ router.put(
       logger.info(
         'Failed to get because need to include grocery_id in body and at least one thing you want to change: name, quantity, price, category, or bought'
       );
-      res
-        .status(400)
-        .send({
-          message:
-            'Failed to get because need to include grocery_id in body and at least one thing you want to change: name, quantity, price, category, or bought',
-        });
+      res.status(400).send({
+        message:
+          'Failed to get because need to include grocery_id in body and at least one thing you want to change: name, quantity, price, category, or bought',
+      });
     }
   }
 );
